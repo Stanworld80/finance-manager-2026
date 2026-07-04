9ac8c2d34bfca03840f18980f7990daa569800ce
Stanworld <stanworld@gmail.com>
2026-07-04 21:40:51 +0200

    ci: trigger CD pipeline with dummy change in main_dev.dart

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// Trigger CD pipeline
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
