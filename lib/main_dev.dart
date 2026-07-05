ecf52f41edd21bcc2b9ca953b7954d0018da4d4e
Stanworld <stanworld@gmail.com>
2026-07-05 11:57:19 +0200

    ci: trigger CD pipeline with dummy comment v5

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// Trigger CD pipeline again v4
+// Trigger CD pipeline again v5
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
